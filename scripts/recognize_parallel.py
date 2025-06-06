55ac56c3b0c8315bdab8b40648671a2210df865e
Vladislav Stankov <stankov344@gmail.com>
2025-06-06 20:50:19 +0200

    update recognize task

--- scripts/recognize_parallel.py
+++ scripts/recognize_parallel.py
@@ -1,10 +1,6 @@
-import subprocess
-import time
-
-from pathlib import Path
-
 import hydra
 import pandas as pd
+from parczech.slurm_utils import push_job, setup_slurm
 
 
 def divide_into_chunks(lst, n_parts):
@@ -15,66 +11,47 @@ def divide_into_chunks(lst, n_parts):
         parts[i % n_parts].append(item)
     return parts
 
-def get_best_gpu(priority_list):
-    """Check if a job with specified GPU memory can start immediately."""
-    for gpu in priority_list:
-        try:
-            cmd = ["sinfo", "-o", "%N %G", "--state=idle"]
-            result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=False)
-            if gpu in result.stdout:
-                return gpu
-        except Exception as e:
-            print(f"Error checking job submission: {e}")
-            continue
-    return None
-
 
 @hydra.main(config_path="../configs", config_name="recognize_parallel")
 def main(cfg):
-    Path(cfg.bash_script_dir).mkdir(parents=True, exist_ok=True)
-    Path(cfg.slurm_logs_dir).mkdir(parents=True, exist_ok=True)
+    template = setup_slurm(
+        slurm_scripts_dir=cfg.bash_script_dir,
+        slurm_log_dir=cfg.slurm_logs_dir,
+        template_path=cfg.template_script,
+    )
 
     metadata_df = pd.read_csv(cfg.metadata_df, sep="\t").sort_values("cntFiles", ascending=False)
     unique_links = metadata_df["repositoryUrl"].unique()
 
-    with open(cfg.template_script) as f:
-        template = f.read()
-
-    used_gpus = []
-    used_gpus_index = 0
     for i, link_chunk in enumerate(divide_into_chunks(unique_links, cfg.n_jobs)):
         if cfg.n_debug is not None and cfg.n_debug > 0 and i > cfg.n_debug:
             print(f"Debug mode. Processing only {cfg.n_debug} link.")
             break
         script_name = f"recognize_{i}.sh"
-        links = '"[' + ",".join(link_chunk) + ']"'
-        gpu = get_best_gpu(cfg.gpu_priority)
 
-        # If no GPU is available, request for already requested GPU
-        # since will be available in 20 hours
-        if gpu is None:
-            gpu = used_gpus[used_gpus_index]
-            used_gpus_index = (used_gpus_index + 1) % len(used_gpus)
-        else:
-            used_gpus.append(gpu)
+        script_params = "overwrite_recognized={ore} links={l} output_folder={of} n_debug={nd}".format(
+            ore=cfg.params.overwrite_recognized,
+            l='"[' + ",".join(link_chunk) + ']"',
+            of=cfg.params.output_folder,
+            nd=cfg.params.n_debug,
+        )
 
         instance = template.format(
-            id=i,
-            gpu=gpu,
-            links=links,
-            n_debug=cfg.n_debug,
-            config=cfg.job_config,
-            slurm_logs_dir=cfg.slurm_logs_dir,
-            output_folder=cfg.output_folder,
-            overwrite_recognized=cfg.overwrite_recognized,
+            job_name=cfg.job_name,
+            job_id=i,
+            cpus=cfg.cpus,
+            mem=cfg.mem,
+            n_gpus=cfg.n_gpus,
+            script=cfg.script,
+            params=script_params,
         )
-        with open(Path(cfg.bash_script_dir, script_name), "w") as f:
-            f.write(instance)
 
-        cmd = ["sbatch", Path(cfg.bash_script_dir, script_name).as_posix()]
-        result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=False)
-        print(result.stdout, end="")
-        time.sleep(2)
+        push_job(
+            script_dir=cfg.bash_script_dir,
+            script_name=script_name,
+            script_content=instance,
+        )
+
     print(f"Done. Debug mode is {cfg.n_debug}.")
 
 if __name__ == "__main__":
