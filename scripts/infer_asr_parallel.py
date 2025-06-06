f6829839322955f74dfb4cdcefed40469eec8d9a
Vladislav Stankov <stankov344@gmail.com>
2025-06-06 22:48:05 +0200

    update infer asr parallel script

--- scripts/infer_asr_parallel.py
+++ scripts/infer_asr_parallel.py
@@ -1,17 +1,16 @@
-import subprocess
 from pathlib import Path
 
 import hydra
+from parczech.slurm_utils import push_job, setup_slurm
 
 
 @hydra.main(config_path="../configs", config_name="infer_asr_parallel")
 def main(cfg):
-    Path(cfg.bash_script_dir).mkdir(parents=True, exist_ok=True)
-    Path(cfg.slurm_logs_dir).mkdir(parents=True, exist_ok=True)
-
-    with open(cfg.template_script) as f:
-        template = f.read()
-
+    template = setup_slurm(
+        slurm_scripts_dir=cfg.bash_script_dir,
+        slurm_log_dir=cfg.slurm_logs_dir,
+        template_path=cfg.template_script,
+    )
 
     for parquet_file in Path(cfg.parquet_dir).glob("*.parquet"):
         script_name = f"recognize_{parquet_file.stem}.sh"
@@ -35,12 +34,12 @@ def main(cfg):
             script=cfg.script,
             params=script_params,
         )
-        with open(Path(cfg.bash_script_dir, script_name), "w") as f:
-            f.write(instance)
 
-        cmd = ["sbatch", Path(cfg.bash_script_dir, script_name).as_posix()]
-        result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=False)
-        print(result.stdout, end="")
+        push_job(
+            script_dir=cfg.bash_script_dir,
+            script_name=script_name,
+            script_content=instance,
+        )
     print("Done.")
 
 if __name__ == "__main__":
