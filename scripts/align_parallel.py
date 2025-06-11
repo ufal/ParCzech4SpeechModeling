29f9affb2110211511b5a20271ce11443af65660
Vladislav Stankov <stankov344@gmail.com>
2025-06-11 08:36:57 +0200

    unify align job

--- scripts/align_parallel.py
+++ scripts/align_parallel.py
@@ -1,41 +1,47 @@
-import subprocess
 from pathlib import Path
 
 import hydra
+from parczech.slurm_utils import push_job, setup_slurm
 
 
 @hydra.main(config_path="../configs", config_name="align_parallel")
 def main(cfg):
-    Path(cfg.bash_script_dir).mkdir(parents=True, exist_ok=True)
-    Path(cfg.slurm_logs_dir).mkdir(parents=True, exist_ok=True)
     Path(cfg.output_directory).mkdir(parents=True, exist_ok=True)
-
-    with open(cfg.template_script) as f:
-        template = f.read()
-
-    job_id = 0
+    template = setup_slurm(
+        slurm_scripts_dir=cfg.bash_script_dir,
+        slurm_log_dir=cfg.slurm_logs_dir,
+        template_path=cfg.template_script,
+    )
 
     for year_dir in Path(cfg.recognized_base_path).iterdir():
         for month_dir in year_dir.iterdir():
-                year = year_dir.name
-                month = month_dir.name
+            year = year_dir.name
+            month = month_dir.name
 
-                script_name = f"recognize_{year}{month}.sh"
+            id = f"{year}_{month}"
+            script_name = f"{id}.sh"
+            params = "year={y} month={m} model_names={mn} output_directory={od}".format(  # noqa: UP032
+                y=year,
+                m=month,
+                mn=cfg.model_names,
+                od=cfg.output_directory,
+            )
 
-                instance = template.format(
-                    id=job_id,
-                    year=year,
-                    month=month,
-                    model_names=cfg.model_names,
-                    output_directory=cfg.output_directory,
-                    slurm_logs_dir=cfg.slurm_logs_dir,
-                )
-                with open(Path(cfg.bash_script_dir, script_name), "w") as f:
-                    f.write(instance)
-                cmd = ["sbatch", Path(cfg.bash_script_dir, script_name).as_posix()]
-                result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, check=False)
-                print(result.stdout, end="")
-                job_id += 1
+            instance = template.format(
+                job_id=id,
+                job_name=cfg.job_name,
+                cpus=cfg.cpus,
+                mem=cfg.mem,
+                script=cfg.script,
+                params=params,
+            )
+
+            push_job(
+                script_dir=cfg.bash_script_dir,
+                script_name=script_name,
+                script_content=instance,
+            )
+    print("Done.")
 
 
 if __name__ == "__main__":
